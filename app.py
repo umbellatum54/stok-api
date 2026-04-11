0a7afa31d4944215f16d218fd09a90c346cee083
umbellatum54 <gunaygaspik@gmail.com>
2026-04-11 18:14:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+stoklar = []
+
 @app.route("/")
 def home():
-    return "Stok API çalışıyor 🚀"
+    return """
+    <h2>Stok Giriş Paneli</h2>
+    <form action="/ekle" method="post">
+        Ürün Adı: <input type="text" name="urun"><br><br>
+        Adet: <input type="number" name="adet"><br><br>
+        <button type="submit">Kaydet</button>
+    </form>
+    """
+
+@app.route("/ekle", methods=["POST"])
+def ekle():
+    urun = request.form.get("urun")
+    adet = request.form.get("adet")
+
+    stoklar.append({
+        "urun": urun,
+        "adet": adet
+    })
+
+    return f"{urun} eklendi! <br><a href='/'>Geri dön</a>"
+
+@app.route("/stok")
+def stok():
+    return jsonify(stoklar)
+
+if __name__ == "__main__":
+    app.run()
