ffa77c735528568fdb1aa4754931882d3cae6187
umbellatum54 <gunaygaspik@gmail.com>
2026-04-12 17:30:41 +0300

    Update stok.py

--- routes/stok.py
+++ routes/stok.py
@@ -1,30 +1,51 @@
-from flask import Blueprint, render_template
-from db import get_conn
+@stok_bp.route('/urun-ekle', methods=['POST'])
+def urun_ekle():
+    ad = request.form['urun']
+    renk = request.form['renk']
 
-stok_bp = Blueprint('stok', __name__)
-
-@stok_bp.route('/stok')
-def stok():
     conn = get_conn()
     cur = conn.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS urunler(id SERIAL PRIMARY KEY, ad TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS depo(id SERIAL PRIMARY KEY, ad TEXT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS hareket(id SERIAL PRIMARY KEY, urun TEXT, adet INT, tarih TEXT)")
-
-    cur.execute("SELECT COUNT(*) FROM urunler")
-    stok_sayisi = cur.fetchone()[0]
-
-    cur.execute("SELECT COUNT(*) FROM depo")
-    depo_sayisi = cur.fetchone()[0]
-
-    cur.execute("SELECT * FROM hareket ORDER BY id DESC LIMIT 10")
-    logs = cur.fetchall()
+    cur.execute("INSERT INTO urunler (ad) VALUES (%s)", (ad,))
+    conn.commit()
 
     cur.close()
     conn.close()
 
-    return render_template("stok.html",
-                           stok_sayisi=stok_sayisi,
-                           depo_sayisi=depo_sayisi,
-                           logs=logs)
+    return redirect('/stok')
+
+
+@stok_bp.route('/stok-giris', methods=['POST'])
+def stok_giris():
+    urun = request.form['urun']
+    adet = request.form['adet']
+
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO hareket (urun, adet, tarih) VALUES (%s,%s,%s)",
+                (urun, adet, datetime.now()))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return redirect('/stok')
+
+
+@stok_bp.route('/stok-cikis', methods=['POST'])
+def stok_cikis():
+    urun = request.form['urun']
+    adet = request.form['adet']
+
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO hareket (urun, adet, tarih) VALUES (%s,%s,%s)",
+                (urun, -int(adet), datetime.now()))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return redirect('/stok')
