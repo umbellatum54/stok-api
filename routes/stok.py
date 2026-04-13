8652b20722865d601af69146b4ba57fecda1f094
umbellatum54 <gunaygaspik@gmail.com>
2026-04-13 12:01:10 +0300

    Update stok.py

--- routes/stok.py
+++ routes/stok.py
@@ -4,21 +4,31 @@ from datetime import datetime
 stok_bp = Blueprint('stok', __name__)
 
 
+# ANA SAYFA
 @stok_bp.route('/stok')
 def stok():
     return render_template("stok.html")
 
 
+# ÜRÜN KARTLARI
+@stok_bp.route('/urunler')
+def urunler():
+    return render_template("urunler.html")
+
+
+# STOK GİRİŞ
 @stok_bp.route('/stok-giris')
-def stok_giris_page():
+def stok_giris():
     return render_template("stok_giris.html")
 
 
+# STOK ÇIKIŞ
 @stok_bp.route('/stok-cikis')
-def stok_cikis_page():
+def stok_cikis():
     return render_template("stok_cikis.html")
 
 
+# STOK ÖZET
 @stok_bp.route('/stok-ozet')
-def stok_ozet_page():
+def stok_ozet():
     return render_template("stok_ozet.html")
