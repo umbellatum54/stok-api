6a90b0433144e722fc0f808c848143de509c816b
umbellatum54 <gunaygaspik@gmail.com>
2026-04-13 18:38:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ from routes.stok import stok_bp
 app = Flask(__name__)
 app.secret_key = "secret123"
 
+# 🔥 DEBUG EKLEDİK (önemli)
+app.config["DEBUG"] = True
+
+# 🔥 REGISTER
 app.register_blueprint(auth_bp)
 app.register_blueprint(stok_bp)
+
+# TEST ROUTE (çok önemli)
+@app.route("/test")
+def test():
+    return "ÇALIŞIYOR"
 
 if __name__ == "__main__":
     app.run()
